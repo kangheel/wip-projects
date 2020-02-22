b088817d515dfce997a98f74b2b8e6ff90f5b8a5
Jaden-Lee <jadenlee77@gmail.com>
2020-02-21 22:40:21 -0800

    calculates the number of NxN linear node

---
+++ Node_Connection.java
@@ -0,0 +1,78 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+public class Node_Connection {
+    private static HashMap<Node, ArrayList<Node>> list = new HashMap();
+    public static void main(String[] args) {
+        Node[][] array = new Node[100][100];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                array[i][j] = new Node(i,j);
+                list.put(array[i][j],new ArrayList<Node>());
+            }
+        }
+        
+        // printArray(array);
+        int count = 0;
+        for (Node[] nodearr1: array) {
+            for (Node node1: nodearr1) {
+                for (Node[] nodearr2: array) {
+                    for (Node node2: nodearr2) {
+                        if (! node1.equals(node2)) {
+                            double denom = 1.0*(node1.getX()-node2.getX());
+                            if (denom == 0) {
+                                if (! list.get(node1).contains(node2)) {
+                                    if (node1.getY()-1 == node2.getY() || node1.getY()+1 == node2.getY()) {
+                                        list.get(node1).add(node2);
+                                        list.get(node2).add(node1);
+                                        count++;
+                                        // System.out.println(list + "\n");
+                                    }
+                                }
+                            }
+                            else {
+                                double slope = (node1.getY()-node2.getY())/denom;
+                                if (slope == 0) {
+                                    if (! list.get(node1).contains(node2)) {
+                                        if (node1.getX()-1 == node2.getX() || node1.getX()+1 == node2.getX()) {
+                                            list.get(node1).add(node2);
+                                            list.get(node2).add(node1);
+                                            count++;
+                                            // System.out.println(list + "\n");
+                                        }
+                                    }
+                                }
+                                else {
+                                    if (! list.get(node1).contains(node2)) {
+                                        if (slope == 1.0 || slope == -1.0) {
+                                            if ((node1.getY()-1 == node2.getY() || node1.getY()+1 == node2.getY()) && (node1.getX()-1 == node2.getX() || node1.getX()+1 == node2.getX())) {
+                                                list.get(node1).add(node2);
+                                                list.get(node2).add(node1);
+                                                count++;
+                                                // System.out.println(list + "\n");
+                                            }
+                                        }
+                                        else {
+                                            list.get(node1).add(node2);
+                                            list.get(node2).add(node1);
+                                            count++;
+                                            // System.out.println(list + "\n");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(list + "\n");
+        System.out.println(count);
+    }
+    public static void printArray(Node[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.println(array[i][j]);
+            }
+        }
+    }
+}
--- node.java
+++ node.java
@@ -1,69 +1,16 @@
-import java.util.HashMap;
-public class Node_Connection {
-    private static HashMap<Node, Node> list = new HashMap<>();
-    public static void main(String[] args) {
-        Node[][] array = new Node[3][3];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                array[i][j] = new Node(i,j);
-            }
-        }
-        
-        printArray(array);
-        int count = 0;
-        for (Node[] nodearr1: array) {
-            for (Node node1: nodearr1) {
-                for (Node[] nodearr2: array) {
-                    for (Node node2: nodearr2) {
-                        double denom = 1.0*(node1.getX()-node2.getX());
-                        if (denom == 0) {
-                            if (list.getOrDefault(node1,new Node(-1,-1)).equals(new Node(-1,-1))) {
-                                    list.put(node1, node2);
-                                    list.put(node2, node1);
-                                    count++;
-                            }
-                            else {
-                                System.out.println(node1);
-                                System.out.println(node2);
-                            }
-                        }
-                        else {
-                            double slope = (node1.getY()-node2.getY())/denom;
-                            if (slope == 0) {
-                                if (list.getOrDefault(node1,new Node(-1,-1)).equals(new Node(-1,-1))) {
-                                    list.put(node1, node2);
-                                    list.put(node2, node1);
-                                    count++;
-                                }
-                                else {
-                                    System.out.println(node1);
-                                    System.out.println(node2);
-                                }
-                            }
-                            else {
-                                if (list.getOrDefault(node1,new Node(-1,-1)).equals(new Node(-1,-1))) {
-                                    list.put(node1, node2);
-                                    list.put(node2, node1);
-                                    count++;
-                                }
-                                else {
-                                    System.out.println(node1);
-                                    System.out.println(node2);
-                                }
-                            }
-                        }
-                        System.out.println(list);
-                    }
-                }
-            }
-        }
-        System.out.println(count);
+public class Node {
+    private int x;
+    private int y;
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
-    public static void printArray(Node[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[0].length; j++) {
-                System.out.println(array[i][j]);
-            }
-        }
+    public String toString() {
+        return "<"+x+","+y+">";
+    }
+    public int getX() {return x;}
+    public int getY() {return y;}
+    public boolean equals(Node node) {
+        return (x == node.x) && (y == node.y);
     }
 }
